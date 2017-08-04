cdc9b0f787445cfe7508c450391fa9287cf4a35c
Oliver Gutsche <oguatworld@gmail.com>
2017-08-04 14:12:18 -0500

    added date_time and username to output filename to avoid conflicts from previous runs and other user runs

--- src/main/scala/org/dianahep/sparkrootapplications/examples/ReductionExampleApp.scala
+++ src/main/scala/org/dianahep/sparkrootapplications/examples/ReductionExampleApp.scala
@@ -18,6 +18,10 @@ import org.dianahep.sparkrootapplications.defs.aodpublic._
 import org.dianahep.histogrammar._
 import org.dianahep.histogrammar.ascii._
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 // for casting
 case class Event(muons: Seq[RecoLeafCandidate]);
 
@@ -48,10 +52,23 @@ object ReductionExampleApp {
     val dsDiMuons = dsMuons.filter(_.muons.length>=2)
       .flatMap({e: Event => for (i <- 0 until e.muons.length; j <- 0 until e.muons.length) yield buildDiCandidate(e.muons(i), e.muons(j))})
     dsDiMuons.show
-    dsDiMuons.write.format("parquet").save("file:/tmp/testReduced.parquet")
+    
+    // get current user name
+    val userName = System.getProperty("user.name")
+
+    // get current date and time
+    val now = Calendar.getInstance().getTime();
+    val dateFormatter = new SimpleDateFormat("YYMMdd_HHmmss");
+    val date = dateFormatter.format(now)
+
+    // create filenames
+    val parquetFilename = "file:/tmp/" + userName + "_" + date + "_testReduced.parquet"
+    val jsonFilename = "/tmp/" + userName + "_" + date + "_testBundle.json"
+
+    dsDiMuons.write.format("parquet").save(parquetFilename)
     val filled = dsDiMuons.rdd.aggregate(emptyDiCandidate)(new Increment, new Combine);
     filled("pt").println;
-    filled.toJsonFile("/tmp/testBundle.json")
+    filled.toJsonFile(jsonFilename)
 
     // stop the session/context
     spark.stop
